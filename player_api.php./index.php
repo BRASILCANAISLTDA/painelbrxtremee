f5a8674a5ad74f903d39b4cf4a6825b0a25c681b
BRASILCANAISLTDA <j82tym8qhd@privaterelay.appleid.com>
2026-03-08 10:15:52 -0300

    Add user_info and server_info JSON structure
    Added user and server information in JSON format.

---
+++ player_api.php./index.php
@@ -0,0 +1,21 @@
+{
+  "user_info": {
+    "username": "usuario",
+    "password": "senha",
+    "auth": 1,
+    "status": "Active",
+    "exp_date": "4102444800",
+    "is_trial": "0",
+    "active_cons": "0",
+    "max_connections": "1",
+    "allowed_output_formats": ["m3u8", "ts", "mp4"]
+  },
+  "server_info": {
+    "url": "seu-usuario.github.io",
+    "port": "80",
+    "https_port": "443",
+    "server_protocol": "http",
+    "timezone": "America/Sao_Paulo",
+    "timestamp_now": 1704067200
+  }
+}
